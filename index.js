b3ae3bcb7b3b1769ef5de779f63413775dfce16e
+branden <+branden_ott@outlook.com+>
2022-01-19 17:21:41 -0500

    commit tree 1d0e49b7847862247d4885f80132a762a97b267b

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const str = word.toLowerCase()
+  const length = str.length
+  for (let i = 0; i < length/2; i ++) {
+    if (str[i] !== str[length -1 - i]) {
+      return false
+    }
+  }return true;
+
 }
 
 /* 
   Add your pseudocode here
+  put code in lower case
+  get string length
+  create for loop ++ counter for each letter over 2
+  if condition if letter in possition i != -i, return false else return true
 */
 
 /*
   Add written explanation of your solution here
+  After putting all letters in the same case, We create a for loop where the counter
+  represents the possition of the letter.We then check if the position of the letter mathes the letter in its mirrored possition
+  IF yes, return true if not, return false 
 */
 
 // You can run `node index.js` to view these console logs
